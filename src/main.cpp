f5f4535f2e3300f642c388dfae77afd47c29d82b
g-g-gerin <124514292+g-g-gerin@users.noreply.github.com>
2024-04-20 11:36:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
+#include "Automata.h"
 
+int main() {
+    Automata automata;
+    automata.on();
+    automata.getMenu();
+    automata.coin(50);
+    automata.choice(2); // Предположим, что пользователь выбрал кофе (2)
+    automata.off();
+
+    return 0;
+}
