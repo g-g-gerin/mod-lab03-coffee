bb41226391366e1d7417e66ea5bd2dd981eb3ce1
g-g-gerin <124514292+g-g-gerin@users.noreply.github.com>
2024-04-20 11:37:49 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,11 @@
-// Copyright 2022 GHA Test Team
-
-#include <gtest/gtest.h>
 #include "Automata.h"
+#include <gtest/gtest.h>
+
+TEST(AutomataTest, TestOnOff) {
+    Automata automata;
+    EXPECT_EQ(automata.getState(), STATES::OFF);
+    automata.on();
+    EXPECT_EQ(automata.getState(), STATES::WAIT);
+    automata.off();
+    EXPECT_EQ(automata.getState(), STATES::OFF);
+}
